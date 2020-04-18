9bc34c28548e47ac690a4806d1303c416362fa4a
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-04-18 10:43:04 +0900

    Tetromino 최종

--- Tetromino/tetromino.c
+++ Tetromino/tetromino.c
@@ -32,13 +32,14 @@ int main() {
 	shape2();
 	shape3();
 	shape4();
-	/*
 	shape5();
-	*/
+	
 	printf("%d", max);
 }
 
 void shape1() {
+
+	//0000
 	for (int i = 0; i < N; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 3; j++) {
@@ -48,10 +49,14 @@ void shape1() {
 		}
 	}
 
-	for (int i = 0; i < N; i++) {
+	//0
+	//0
+	//0
+	//0
+	for (int i = 0; i < N - 3; i++) {
 		int result = 0;
-		for (int j = 0; j < M - 3; j++) {
-			result = board[j][i] + board[j + 1][i] + board[j + 2][i] + board[j + 3][i];
+		for (int j = 0; j < M; j++) {
+			result = board[i][j] + board[i + 1][j] + board[i + 2][j] + board[i + 3][j];
 			if (result > max)
 				max = result;
 		}
@@ -59,6 +64,9 @@ void shape1() {
 }
 
 void shape2() {
+
+	//00
+	//00
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -70,6 +78,10 @@ void shape2() {
 }
 
 void shape3() {
+
+	//0
+	//0
+	//00
 	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -79,6 +91,9 @@ void shape3() {
 		}
 	}
 
+	//00
+	//0
+	//0
 	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -88,6 +103,8 @@ void shape3() {
 		}
 	}
 
+	//000
+	//0
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
@@ -97,6 +114,8 @@ void shape3() {
 		}
 	}
 
+	//000
+	//  0
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
@@ -106,15 +125,21 @@ void shape3() {
 		}
 	}
 
-	for (int i = 2; i < N; i++) {
+	// 0
+	// 0
+	//00
+	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
-			result = board[i][j] + board[i][j + 1] + board[i - 1][j + 1] + board[i - 2][j + 1];
+			result = board[i + 2][j] + board[i + 2][j + 1] + board[i + 1][j + 1] + board[i][j + 1];
 			if (result > max)
 				max = result;
 		}
 	}
 
+	//00
+	// 0
+	// 0
 	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -124,15 +149,19 @@ void shape3() {
 		}
 	}
 
+	//  0
+	//000
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
-			result = board[i][j] + board[i][j + 1] + board[i][j + 2] + board[i + 1][j + 2];
+			result = board[i + 1][j] + board[i + 1][j + 1] + board[i + 1][j + 2] + board[i][j + 2];
 			if (result > max)
 				max = result;
 		}
 	}
 
+	//0
+	//000
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
@@ -144,6 +173,10 @@ void shape3() {
 }
 
 void shape4() {
+
+	//0
+	//00
+	// 0
 	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -153,6 +186,9 @@ void shape4() {
 		}
 	}
 
+	// 0
+	//00
+	//0
 	for (int i = 0; i < N - 2; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 1; j++) {
@@ -162,6 +198,8 @@ void shape4() {
 		}
 	}
 
+	//00
+	// 00
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
@@ -171,10 +209,55 @@ void shape4() {
 		}
 	}
 
+	// 00
+	//00
 	for (int i = 0; i < N - 1; i++) {
 		int result = 0;
 		for (int j = 0; j < M - 2; j++) {
 			result = board[i + 1][j] + board[i + 1][j + 1] + board[i][j + 1] + board[i][j + 2];
+			if (result > max)
+				max = result;
+		}
+	}
+}
+
+void shape5() {
+	
+	//で
+	for (int i = 0; i < N - 1; i++) {
+		int result = 0;
+		for (int j = 0; j < M - 2; j++) {
+			result = board[i + 1][j] + board[i + 1][j + 1] + board[i + 1][j + 2] + board[i][j + 1];
+			if (result > max)
+				max = result;
+		}
+	}
+
+	//ぬ
+	for (int i = 0; i < N - 1; i++) {
+		int result = 0;
+		for (int j = 0; j < M - 2; j++) {
+			result = board[i][j] + board[i][j + 1] + board[i][j + 2] + board[i + 1][j + 1];
+			if (result > max)
+				max = result;
+		}
+	}
+
+	//っ
+	for (int i = 0; i < N - 2; i++) {
+		int result = 0;
+		for (int j = 0; j < M - 1; j++) {
+			result = board[i][j + 1] + board[i + 1][j + 1] + board[i + 2][j + 1] + board[i + 1][j];
+			if (result > max)
+				max = result;
+		}
+	}
+
+	//た
+	for (int i = 0; i < N - 2; i++) {
+		int result = 0;
+		for (int j = 0; j < M - 1; j++) {
+			result = board[i][j] + board[i + 1][j] + board[i + 2][j] + board[i + 1][j + 1];
 			if (result > max)
 				max = result;
 		}
