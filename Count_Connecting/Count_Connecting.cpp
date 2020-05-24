8f37184a79e1852c25b316995a29e5bc14f16048
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-05-24 11:03:17 +0900

    Count Connecting

---
+++ Count_Connecting/Count_Connecting.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> graph[1001];
+bool visited[1001];
+
+void dfs(int idx);
+
+int main()
+{
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back(b);
+		graph[b].push_back(a);
+	}
+
+	int check = 0;
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i]) {
+			dfs(i);
+			check++;
+		}
+	}
+
+	cout << check;
+
+	return 0;
+}
+
+void dfs(int idx) {
+	visited[idx] = true;
+
+	for (int i = 0; i < graph[idx].size(); i++) {
+		int idx2 = graph[idx][i];
+		if (!visited[idx2])
+			dfs(idx2);
+	}
+}
