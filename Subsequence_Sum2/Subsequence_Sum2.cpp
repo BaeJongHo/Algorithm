8cc5d65e62771ac0991a22473b2fe73fe779cac5
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-07-27 14:45:25 +0900

    Subsequence Sum 2

---
+++ Subsequence_Sum2/Subsequence_Sum2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N, S;
+int arr[20];
+int result = 0;
+
+void subset(int idx, int sum);
+
+int main(void) {
+	cin >> N >> S;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	subset(0, 0);
+
+	cout << result;
+
+	return 0;
+}
+
+void subset(int idx, int sum) {
+	sum += arr[idx];
+
+	if (idx >= N)
+		return;
+
+	if (sum == S)
+		result++;
+
+	subset(idx + 1, sum - arr[idx]);
+	subset(idx + 1, sum);
+}
