ff10ff26a3fb848472f6d47d9b9e34375cd2ebe8
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-08-02 21:49:12 +0900

    Coin 0

---
+++ Coin0/Coin0.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, K;
+vector<int> v;
+int result = 0;
+
+int main()
+{
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	while (K != 0) {
+		int coin = v.back();
+		v.pop_back();
+
+		if (coin > K)
+			continue;
+		
+		int cnt = K / coin;
+		K -= (K / coin) * coin;
+		result += cnt;
+	}
+
+	cout << result;
+
+	return 0;
+}
