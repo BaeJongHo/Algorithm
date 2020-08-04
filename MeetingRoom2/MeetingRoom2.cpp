d66cb9e7401f0c1ce86b1b6f02903d0a36dde27d
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-08-04 16:33:04 +0900

    Meeting Room 2

---
+++ MeetingRoom2/MeetingRoom2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+vector<pair<int, int>> v;
+
+int main()
+{
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(b, a));
+	}
+
+	sort(v.begin(), v.end());
+
+	int pos_start = 0;
+	int cnt = 0;
+
+	for (int i = 0; i < v.size(); i++) {
+		int start = v[i].second;
+		int end = v[i].first;
+
+		if (pos_start <= start) {
+			pos_start = end;
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+
+	return 0;
+}
