f2e42a14788d272763d48d9b192d6c73406b8220
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-08-20 12:15:15 +0900

    Number Card 2

---
+++ NumberCard2/NumberCard2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int a;
+		cin >> a;
+		cout << upper_bound(v.begin(), v.end(), a) - lower_bound(v.begin(), v.end(), a) << " ";
+	}
+
+	return 0;
+}
