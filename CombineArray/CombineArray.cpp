9a7dcd7ba955ca1b382589ebc6c15495cad0c8b4
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-08-30 15:01:54 +0900

    Combine Array

---
+++ CombineArray/CombineArray.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+
+int main()
+{
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	for (int i = 0; i < M; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+
+	return 0;
+}
