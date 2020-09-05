27f502d80037797310d479da34f724a5ee6def3b
BaeJongHo <44634707+BaeJongHo@users.noreply.github.com>
2020-09-05 16:36:48 +0900

    Number Write

---
+++ NumberWrite/NumberWrite.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, k;
+int cnt = 0;
+
+int main()
+{
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(0);
+
+	cin >> N >> k;
+	for (int i = 0; i < N; i++) {
+		string s = to_string(i);
+		cnt += s.size();
+		if (k <= cnt) {
+			cout << s[s.size() - (cnt - k)];
+			return 0;
+		}
+	}
+	cout << -1;
+
+    return 0;
+}
